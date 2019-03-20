7ff6cae3c5ace6234af75fae0ab76c5674c1b28d
tomaszmartin <tomasz.martin@gmail.com>
2019-03-21 00:02:13 +0100

    Comments

--- python/reloader.py
+++ python/reloader.py
@@ -4,7 +4,9 @@ import signal
 import time
 import sys
 
-DONT_KILL = 3
+
+RELOAD = 3
+
 
 def main(arg):
     while True:
@@ -16,22 +18,31 @@ def run_main(arg):
     while True:
         print(f'Spawning new process.')
         exit_code = subprocess.call(['python3', 'reloader.py', 'restart'], close_fds=False)
-        if exit_code != DONT_KILL:
+        # If process returns exit RELOAD
+        # Than spawn another subprocess
+        # Else exit the app
+        if exit_code != RELOAD:
             return exit_code
 
 
 def run_reloader():
     time.sleep(3)
     print('Found changes in file. Killing current subprocess.')
-    sys.exit(DONT_KILL)
+    sys.exit(RELOAD)
 
 
 if __name__ == '__main__':
     arg = sys.argv[1:]
+    # Run the main loop only from subprocesses
+    # not from the main process.
+    # Wont be run at first launch.
     if arg and arg[0] == 'restart':
         t = threading.Thread(target=main, args=[arg])
         t.setDaemon(True)
         t.start()
+        # Looking
         run_reloader()
+    # At first launch start the loop that spawns
+    # Subprocesses with current file.
     else:
         sys.exit(run_main(arg))
