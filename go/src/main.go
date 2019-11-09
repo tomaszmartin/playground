b798b312f1b443df7891ebf3a1e9eaae654d0bfa
tomaszmartin <tomasz.martin@gmail.com>
2019-11-09 15:08:49 +0100

    Add InsertionSort

--- go/src/main.go
+++ go/src/main.go
@@ -1,10 +1,12 @@
 package main
 
-import "fmt"
-import "sorting"
+import (
+	"fmt"
+	"sorting"
+)
 
 func main() {
-	unsorted := []int{1, 9, 7, 5, 2, 8, 6, 4, -10, -100}
+	unsorted := []int{30, 1, 9, 7, 5, 2, 8, 6, 4, -10, 20, -100, 15}
 	fmt.Println("Unsorted: ", unsorted)
-	fmt.Println("Sorted: ", sorting.BubbleSort(unsorted))
+	fmt.Println("Sorted: ", sorting.InsertionSort(unsorted))
 }
--- go/src/sorting/sorting.go
+++ go/src/sorting/sorting.go
@@ -19,3 +19,26 @@ func BubbleSort(items []int) []int {
 	}
 	return items
 }
+
+// InsertionSort sorts a slice of integers using insertion sort algorithm.
+func InsertionSort(items []int) []int {
+	// We consider first item sorted since there
+	// is nothing to the left to compere it with.
+	// That's why we start at 1 position.
+	for i := 1; i < len(items); i++ {
+		// Make a backward pass
+		for j := i; j > 0; j-- {
+			if items[j-1] > items[j] {
+				// If previous item is bigger than current one than
+				// swap places and keep checking them
+				items[j-1], items[j] = items[j], items[j-1]
+			} else {
+				// When previous item is smaller than You know
+				// that the rest must also be sorted
+				// and can break the loop
+				break
+			}
+		}
+	}
+	return items
+}
