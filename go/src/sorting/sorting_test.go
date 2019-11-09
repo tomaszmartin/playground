017f1bdf65cc2524f1b1d709c370ed9ee102a98e
tomaszmartin <tomasz.martin@gmail.com>
2019-11-09 15:09:12 +0100

    Add benchmark for InsertionSort

--- go/src/sorting/sorting_test.go
+++ go/src/sorting/sorting_test.go
@@ -6,23 +6,21 @@ import (
 	"time"
 )
 
-func randomInt(min, max int) int {
-	return min + rand.Intn(max-min)
-}
+type algorithm func([]int) []int
 
-func benchmarkBubbleSort(b *testing.B, n int) {
+func benchmarkSort(b *testing.B, n int, fn algorithm) {
 	items := make([]int, n)
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i <= n-1; i++ {
-		items[i] = randomInt(-100, 100)
+		items[i] = -100 + rand.Intn(200)
 	}
-	BubbleSort(items)
+	fn(items)
 }
 
-func BenchmarkBubbleSort10_000(b *testing.B) {
-	benchmarkBubbleSort(b, 10_000)
+func BenchmarkBubbleSort(b *testing.B) {
+	benchmarkSort(b, 1000, BubbleSort)
 }
 
-func BenchmarkBubbleSort1_000(b *testing.B) {
-	benchmarkBubbleSort(b, 1_000)
+func BenchmarkInsertSort(b *testing.B) {
+	benchmarkSort(b, 1000, InsertionSort)
 }
